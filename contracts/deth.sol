ea63e0482c5197fc5b7affcc57560d281fabceaa
alanckeegan <alan.c.keegan@gmail.com>
2022-01-31 13:29:08 -0500

    blank setup as a reminder

---
+++ contracts/deth.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract deth {
+//    modified shares-based erc20 that uses dollar balance for underlyign eth deposit based on chainlink oracle
+
+    constructor() {
+
+    }
+
+}
